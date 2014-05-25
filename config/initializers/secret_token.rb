967f156e39f7c78ccee2603631ed703ed31b902d
robinmiller <robin.l.heinze@gmail.com>
2014-05-25 08:49:31 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GameBucket::Application.config.secret_key_base = '534877bca7e924bc112128a005c13e194daf5e8b518ecd4a3cd3e6b46ce549f04c464bb4a26ca34f284764fd1dd13c9f4e8017a33c5fac3fd9c764254d412d3f'
